37915c5b5ed2fa9a003350f53e3d5f6287c910b8
Jenny Allar <jennyallar@gmail.com>
2016-05-03 09:36:54 -0600

    updating endpoint and adding instructions

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+// This code makes a GET request and returns data about five states.
+// We've put the name of the state on the page already.
+// Try adding the capital, state bird, how long the state has been in the union,
+// and displaying the state flag for each state on the page.
+
 $(document).ready(function(){
 
 	$.ajax({
-		url: 'https://api.myjson.com/bins/1kmxk',
+		url: 'https://api.myjson.com/bins/274f8',
 		type: 'GET',
 		data: {
 			format: 'json'
@@ -13,7 +18,6 @@ $(document).ready(function(){
 			var states = results.data;
 			for(i=0; i < states.length; i++) {
 				var firstName = states[i].name;
-				var cap = states[i].capital;
 				$('#states').append("<h3>" + firstName + "</h3>");
 			}
 		}
